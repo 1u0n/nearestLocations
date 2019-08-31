952978879a2e9f3a439635814de5f025528390d8
Juan Lopez <juan@jlop.es>
2019-08-31 19:52:45 +0200

    Initial commit, express app skeleton

---
+++ README.md
@@ -0,0 +1,6 @@
+!NEAREST LOCATIONS
+
+Express server that exposes a REST endpoint at `/locations` :
+
+* **input**: an array of natural-language locations
+* **output**: an array of pairs [A,B] where A is a passed location, and B is its closest location in the same array
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const logger = require('morgan');
+
+const locationsRouter = require('./locationsRouter');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use('/locations', locationsRouter);
+
+module.exports = app;
